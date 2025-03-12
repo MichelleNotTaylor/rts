8d4c1ccbdc28ccd22b43005ef5e4e40cb4ee6c2d
Michelle Taylor <michelle.taylor@parchment.com>
2025-03-12 16:22:21 -0500

    Events and refs

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -4,8 +4,13 @@ const EventComponent: React.FC = () => {
         console.log(event)
     }
 
+    const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+        console.log("I'm being dragged");
+    }
+
     return<div>
         <input onChange={onChange} />
+        <div draggable onDragStart={onDragStart}>Drag me!</div>
     </div>
 };
 
